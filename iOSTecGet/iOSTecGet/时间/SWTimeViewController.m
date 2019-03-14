c5be7ac5c7c294cdba9c9053b1f123d8dd7f7abc
lswfly123 <1439927138@qq.com>
2019-03-14 16:36:53 +0800

    修改了时间模块

--- iOSTecGet/iOSTecGet/时间/SWTimeViewController.m
+++ iOSTecGet/iOSTecGet/时间/SWTimeViewController.m
@@ -24,46 +24,36 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     
+//    // 1.NSDate和NSString之间的相互转化
 //    [self transferStringToDate];
+//
+//    // 2.计算给定两个日期之间相差多少天
+//    NSInteger days = [self daysByDate:@"2019-12-28 00:00:00"];
+//    NSLog(@"距离考试还有%ld天",days);
     
-    // 计算给定两个日期之间相差多少天
-    NSInteger days = [self daysByDate:@"2019-12-28 00:00:00"];
-    NSLog(@"距离考试还有%ld天",days);
+    // 3.由拼接的字符串转化为带格式的日期字符串
+    NSString *currentDateStr = [self translateOriginalFormater:@"yyyyMMddHHmmss" toCurrentFormatter:@"yyyy-MM-dd HH:mm:ss" withDateStr:@"20181015123452"];
+    NSLog(@"转化后的DateStr:%@",currentDateStr);
     
+    // 4.得到年月日时分秒
+    NSString *dateString = [self getYearStringByFormatedString:@"MM" date:[NSDate date]];
+    NSLog(@"得到年:%@",dateString);
 }
 
 // 字符串转NSDate时，NSDate一定是零时区
 - (void)transferStringToDate{
-    
-//    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-//
-//    formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
-//
-//    NSDate *newDate = [formatter dateFromString:@"1986-03-28 06:11:00"];
-//
-//    NSLog(@"newDate = %@", newDate);
-//
+
     // 默认是零时区
     NSDate *date = [NSDate date];
-    NSLog(@"默认的date = %@",date);
     
-//    //实例化一个NSDateFormatter对象
-//    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-//    //设定时间格式,这里可以设置成自己需要的格式
-//    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-//    //用[NSDate date]可以获取系统当前时间
-//    NSString *currentDateStr = [dateFormatter stringFromDate:[NSDate date]];
-//    //输出格式为：2010-10-27 10:22:13
-//    NSLog(@"%@",currentDateStr);
-    
-    
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    
-    [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss Z"];
-    
-    NSDate *newDate = [formatter dateFromString:@"2016-12-07 14:06:24 +0000"];
-    
-    NSLog(@"newDate = %@", newDate);
+    //实例化一个NSDateFormatter对象
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    //设定时间格式,这里可以设置成自己需要的格式
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    //用[NSDate date]可以获取系统当前时间
+    NSString *currentDateStr = [dateFormatter stringFromDate:date];
+    //输出格式为：2010-10-27 10:22:13
+    NSLog(@"%@",currentDateStr);
 }
 
 
@@ -81,4 +71,31 @@
     NSDateComponents *comps = [gregorian components:unitFlags fromDate:now toDate:oldDate options:0];
     return [comps day];
 }
+
+// 将yyyyMMddHHmmss格式转化为yyyy-MM-dd HH:mm:ss
+- (NSString *)translateOriginalFormater:(NSString *)lastDateFormatter toCurrentFormatter:(NSString *)currentDateFormatter withDateStr:(NSString *)dateStr{
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+//    NSString *lastDateFormatter = @"yyyyMMddHHmmss";
+//    NSString *currentDateFormatter = @"yyyy-MM-dd HH:mm:ss";
+    [formatter setDateFormat:lastDateFormatter];
+    
+    [formatter setTimeZone:[NSTimeZone localTimeZone]];
+    NSDate *addDate = [formatter dateFromString:dateStr];
+    
+    NSDateFormatter *formatter1 = [[NSDateFormatter alloc]init];
+    [formatter1 setDateFormat:currentDateFormatter];
+    NSString *strDate = [formatter1 stringFromDate:addDate];
+    return strDate;
+}
+
+// 得到年/月/日/时/分/秒
+- (NSString *)getYearStringByFormatedString:(NSString *)formatedString date:(NSDate *)date{
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:formatedString];
+    NSString *strDate = [formatter stringFromDate:date];
+    return strDate;
+}
+// 得到月
 @end
---
+++ iOSTecGet/iOSTecGet/时间/SWTimerViewController.m
@@ -0,0 +1,21 @@
+//
+//  SWTimerViewController.m
+//  iOSTecGet
+//
+//  Created by 信息科技部 on 2019/3/14.
+//  Copyright © 2019 信息科技部. All rights reserved.
+//
+
+#import "SWTimerViewController.h"
+
+@interface SWTimerViewController ()
+
+@end
+
+@implementation SWTimerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+@end
