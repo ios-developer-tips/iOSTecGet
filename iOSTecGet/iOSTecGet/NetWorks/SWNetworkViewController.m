f7392b8ce72bc90a5c34d85f46a232511c0848bb
lswfly123 <1439927138@qq.com>
2019-04-17 16:17:07 +0800

    添加了网络请求工具类

---
+++ iOSTecGet/iOSTecGet/NetWorks/SWNetworkViewController.m
@@ -0,0 +1,32 @@
+//
+//  SWNetworkViewController.m
+//  iOSTecGet
+//
+//  Created by 信息科技部 on 2019/4/17.
+//  Copyright © 2019 信息科技部. All rights reserved.
+//
+
+#import "SWNetworkViewController.h"
+
+@interface SWNetworkViewController ()
+
+@end
+
+@implementation SWNetworkViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
